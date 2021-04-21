f072ca03382e8ca9ca3e3961b55a41cf1fb241d2
hwakyeom <hwakyeom@icloud.com>
2021-04-22 02:15:34 +0900

    (update)#1: Creating A World
    210422:0215

--- project/src/main.js
+++ project/src/main.js
@@ -1,5 +1,48 @@
 "use strict";
 
-function init() {}
+class World {
+  constructor(context = "2d", canvasSelector = "canvas") {
+    this._canvas = document.querySelector(canvasSelector);
+    this._ctx = this._canvas.getContext(context);
+    this._canvas && this._ctx //
+      ? (this._work = true)
+      : (this._work = false);
+    this.init();
+  }
+  init() {
+    this.resize();
+    this.addEventListeners();
+  }
+  resize() {
+    this._canvas.width = window.innerWidth;
+    this._canvas.height = window.innerHeight;
+  }
+  addEventListeners() {
+    window.addEventListener("resize", () => this.resize());
+  }
+  update() {
+    this._ctx.beginPath();
+    this._ctx.moveTo(20, 20);
+    this._ctx.lineTo(130, 130);
+    this._ctx.stroke();
+    this._ctx.closePath();
+  }
+  get work() {
+    return this._work;
+  }
+  get canvas() {
+    return this._canvas;
+  }
+  get ctx() {
+    return this._ctx;
+  }
+}
 
-init();
+let stage = new World();
+
+function animate() {
+  stage.update();
+  requestAnimationFrame(animate); // 60 fps
+}
+
+animate();
